aa5cb1643ef4484d438760d27f666c3cf66d1444
Martín <magadiflo@gmail.com>
2023-01-24 09:38:59 -0500

    Cambiando al puerto especificado en cada microfrontend remoto

--- projects/mf-shell/webpack.config.js
+++ projects/mf-shell/webpack.config.js
@@ -3,8 +3,8 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfPayment": "http://localhost:4200/remoteEntry.js",
-    "mfShopping": "http://localhost:4200/remoteEntry.js",    
+    "mfPayment": "http://localhost:4202/remoteEntry.js",
+    "mfShopping": "http://localhost:4201/remoteEntry.js",    
   },
 
   shared: {
